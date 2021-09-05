74a312c770ee5a71f07b2e74e64db93a125eb375
Cristian Martinez <cdturizo15@gmail.com>
2021-09-05 18:31:17 -0500

    added map and position locator

--- src/views/scripts/main.js
+++ src/views/scripts/main.js
@@ -1,3 +1,20 @@
-document
-body.append('hello world malparido')
-console.log('hello world nojoda')
+const map = L.map('map-template').setView([10.96854,-74.78132],13)
+const tileURL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+L.tileLayer(tileURL).addTo(map)
+let marker = null
+
+async function getGPS() {
+    response = await fetch("http://taxiflow.zapto.org/gps");
+    coordinates = await response.json();
+    document.getElementById("lat").textContent = coordinates.lat;
+    document.getElementById("lon").textContent = coordinates.lon;
+    document.getElementById("date").textContent = coordinates.date;
+    document.getElementById("time").textContent = coordinates.time;
+    if(marker){
+        map.removeLayer(marker)
+    }
+    marker = L.marker([coordinates.lat,coordinates.lon]).bindPopup('Myposition')
+    map.addLayer(marker)
+}
+
+setInterval(getGPS, 2000);
