db166bc87d577917f680928201a3aa54efce9a9d
Dierickb <dierickbro@gmail.com>
2021-09-07 13:01:59 -0500

    Anexar about u

--- src/views/scripts/main.js
+++ src/views/scripts/main.js
@@ -1,8 +1,7 @@
+const map = L.map('map-template').setView([10.96854,-74.78132],13)
 const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-const attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
-marker = L.marker([-74.7720435, 10.9303097],13)
-const id = 'mapbox/streets-v11'
-var marker = null
+L.tileLayer(tileURL).addTo(map)
+let marker = null
 
 async function getGPS() {
     response = await fetch("http://dierickb.hopto.org/gps");
@@ -12,16 +11,16 @@ async function getGPS() {
     document.getElementById("date").textContent = coordinates.date;
     document.getElementById("time").textContent = coordinates.time;
 
-    const map = L.map('map-template').setView([coordinates.lat, coordinates.lon], 14)
-    L.tileLayer(tileURL, { attribution, id }).addTo(map)
-
     if (marker) {
         map.removeLayer(marker)
     }
     if (coordinates.lat != '') {
-        marker = L.marker([coordinates.lat, coordinates.lon]).addTo(map).bindPopup("s")
+        map.setView([coordinates.lat,coordinates.lon],13)
+        marker = L.marker([coordinates.lat, coordinates.lon],15).addTo(map).bindPopup("s")
+        map.addLayer(marker)
     }
     L.marker([coordinates.lat, coordinates.lon])
 }
 
 setInterval(getGPS, 2000);
+
