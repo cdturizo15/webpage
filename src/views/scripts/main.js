17c8a62ebe94f915ee77dbc95ce2272cf9be837d
Dierickb <dierickbro@gmail.com>
2021-09-06 20:24:34 -0500

    Revert "Merge branch 'master' of https://github.com/cdturizo15/webpage"
    This reverts commit 6df41f552a108aec1822523b47116fd52d6e3d83, reversing
    changes made to 12e26554fe22b087cb06f4c61551909086b8594d.

--- src/views/scripts/main.js
+++ src/views/scripts/main.js
@@ -1,7 +1,8 @@
-const map = L.map('map-template').setView([10.96854,-74.78132],13)
-const tileURL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-L.tileLayer(tileURL).addTo(map)
-let marker = null
+const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+const attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+marker = L.marker([-74.7720435, 10.9303097])
+const id = 'mapbox/streets-v11'
+var marker = null
 
 async function getGPS() {
     response = await fetch("http://dierickb.hopto.org/gps");
@@ -10,15 +11,17 @@ async function getGPS() {
     document.getElementById("lon").textContent = coordinates.lon;
     document.getElementById("date").textContent = coordinates.date;
     document.getElementById("time").textContent = coordinates.time;
-    if(marker){
+
+    const map = L.map('map-template').setView([coordinates.lat, coordinates.lon], 14)
+    L.tileLayer(tileURL, { attribution, id }).addTo(map)
+
+    if (marker) {
         map.removeLayer(marker)
     }
-    if(coordinates.lat!=''){
-        map.setView([coordinates.lat,coordinates.lon],13)
+    if (coordinates.lat != '') {
+        marker = L.marker([coordinates.lat, coordinates.lon]).addTo(map).bindPopup("s")
     }
-    
-    marker = L.marker([coordinates.lat,coordinates.lon]).bindPopup('Myposition')
-    map.addLayer(marker)
+    L.marker([coordinates.lat, coordinates.lon])
 }
 
 setInterval(getGPS, 2000);
