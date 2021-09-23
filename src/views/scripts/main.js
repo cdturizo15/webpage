0b1e82fe9d5ab6a64f1ea76bc051d44942735b06
Cristian Martinez <cdturizo15@gmail.com>
2021-09-23 11:20:18 -0500

    aaa

--- src/views/scripts/main.js
+++ src/views/scripts/main.js
@@ -1,5 +1,6 @@
 const map = L.map('map-template').setView([10.96854, -74.78132], 13)
 const tileURL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+var fetchurl = location.hostname;
 L.tileLayer(tileURL).addTo(map)
 let marker = null
 var sw = 0
@@ -7,9 +8,8 @@ var checkbox = document.querySelector('input[type="checkbox"]');
 var latlngs = [];
 var polyline = null
 
-
 async function getGPS() {
-  response = await fetch('http://localhost/gps');
+  response = await fetch('http://'+fetchurl+'/gps');
   coordinates = await response.json();
   document.getElementById("lat").textContent = coordinates.lat;
   document.getElementById("lon").textContent = coordinates.lon;
@@ -35,14 +35,13 @@ async function getGPS() {
     map.setView([coordinates.lat, coordinates.lon], 17)
   }
   marker = L.marker([coordinates.lat, coordinates.lon]).bindPopup('Myposition')
-  if(coordinates.lat != 0){
+  if(coordinates.lat != ""){
     latlngs.push([coordinates.lat,coordinates.lon])
-
   }  
-  console.log(latlngs)
   polyline = L.polyline(latlngs, {color: 'red',smoothFactor:0.5})
   map.addLayer(polyline)
   map.addLayer(marker)
+
 }
 
 setInterval(getGPS, 2000);
