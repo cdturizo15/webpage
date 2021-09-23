185b2469e820121cb28a3461af75a69123f26da1
Cristian Martinez <cdturizo15@gmail.com>
2021-09-22 21:32:20 -0500

    if changed

--- src/views/scripts/main.js
+++ src/views/scripts/main.js
@@ -31,7 +31,7 @@ async function getGPS() {
       sw = 0
     }
   });
-  if ((coordinates.lat != '' && (sw == 0))) {1
+  if ((coordinates.lat != '' && (sw != 0))) {1
     map.setView([coordinates.lat, coordinates.lon], 17)
   }
   marker = L.marker([coordinates.lat, coordinates.lon]).bindPopup('Myposition')
