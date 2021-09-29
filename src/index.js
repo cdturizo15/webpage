dc4d41f382501461a9b3e11eb3c3e96e9343be81
cdturizo15 <84291609+cdturizo15@users.noreply.github.com>
2021-09-28 20:37:27 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ const child_p = require('child_process')
 const {promisify} = require('util')
 const port = 80
 const connection = mysql.createConnection({
-    host: 'taxiflowdatabase.c0u6vxuknyg3.us-west-2.rds.amazonaws.com', // HOST NAME
-    user: 'taxiflow', // USER NAME
-    database: 'taxiflow', // DATABASE NAME
-    password: 'David5597' // DATABASE PASSWORD
+    host: process.env.HOST,
+    port: 3306,
+    user: process.env.USER,
+    database: 'taxiflow', 
+    password: process.env.PASS
 });
 var lat = '';
 var lon = '';
