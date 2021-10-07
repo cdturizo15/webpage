541931e95b74defdc4a95d2905718dfc282aa2b4
Dierickb <71480347+Dierickb@users.noreply.github.com>
2021-10-07 02:29:09 -0500

    sss

--- udp_server.js
+++ udp_server.js
@@ -6,11 +6,10 @@ const mysql = require('mysql');
 require('dotenv').config()
 
 const connection = mysql.createConnection({
-    host: process.env.HOST,
-    port: 3306,
-    user: process.env.USER,
-    database: 'taxiflow', 
-    password: process.env.PASS
+    host: process.env.HOST, // HOST NAME
+    user: process.env.USER, // USER NAME
+    database: 'taxiflow', // DATABASE NAME
+    password: process.env.PASS // DATABASE PASSWORD
 });
 
 connection.connect(function(error){
@@ -23,17 +22,17 @@ connection.connect(function(error){
 
 
 socket.on('listening', () => {
-    console.log(`UDP server listening on port`, process.env.UDPPORT);
+    console.log(`UDP server listening on port 9000`);
   });
 
 socket.on('message',(message)=>{
     console.log('message: '+ message);
-    const lat = String(message).substr(17,8);
-    const lon = String(message).substr(31,8);
-    const date = String(message).substr(62,11);
-    const time = String(message).substr(73,8);
-    const timestamp = String(message).substr(62,19);
-    const license_plate = String(message).substr(103,7);
+    const lat = String(message).substr(0,7);
+    const lon = String(message).substr(10,8);
+    const date = String(message).substr(21,10);
+    const time = String(message).substr(32,8);
+    const timestamp = String(message).substr(21,19);
+    const license_plate = String(message).substr(44,8);
     console.log(lat);
     console.log(lon);
     console.log(date);
@@ -66,26 +65,6 @@ socket.on('message',(message)=>{
                             location = rows[0]
                             console.log(location); 
                     };
-                    
-                    
-                });
-                const start = '2021-09-28 01:08:18';
-                const end = '2021-09-28 23:08:18';
-                connection.query(`SELECT * FROM taxiflow.location
-                            WHERE timestamp >= '${start}' AND timestamp <= '${end}'`, function(error, rows){
-                    if(error){
-                        throw error;
-                    }else{ 
-                        var latt = [];
-                        for (i in rows) {
-                            latt.push(rows[i].latitude);
-                        }
-                        console.log(latt)
-
-                        console.log(rows[0])
-                        console.log(rows[rows.length - 1])
-
-                    };                    
                 });
             });
         };
@@ -93,3 +72,4 @@ socket.on('message',(message)=>{
 });
 
 socket.bind(process.env.UDPPORT)
+
