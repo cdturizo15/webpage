a00600c48d1a7bdd614fae3b5a8fa88dcb440385
cdturizo15 <84291609+cdturizo15@users.noreply.github.com>
2021-11-16 13:47:28 -0500

    Update udp_server.js

--- udp_server.js
+++ udp_server.js
@@ -40,7 +40,8 @@ socket.on('message',(message)=>{
     startTime =   new Date( startTime.getTime() + ( startTime.getTimezoneOffset()*60000 ) );
 
     var date27 = convertUTCDateToLocalDate(new Date(strDate)) 
-    console.log("Fecha cambio: "+Date.parse(date27));
+    console.log("Fecha cambio: "+date27);
+    console.log("Fecha cambio mili: "+Date.parse(date27));
     console.log("Fecha correcta:"+infoMsg[3])
     console.log("Fecha final:  "+strDate);
     const dateObject2 = new Date(Date.parse(date27));
